82f638c8b3219b9ed59eec2dfc4e49a64f886375
Lamda94 <38790016+Lamda94@users.noreply.github.com>
2021-05-11 12:19:38 -0500

    .

--- script/firebase.js
+++ script/firebase.js
@@ -1,11 +1,11 @@
- // Variable de configuracion de firebase
- var firebaseConfig = {
+// Variable de configuracion de firebase
+var firebaseConfig = {
     apiKey: "AIzaSyDU2uuZLY376VsdF3tv2SvOo_Oarqo7Av8",
     authDomain: "psjs-a8703.firebaseapp.com",
     projectId: "psjs-a8703",
     storageBucket: "psjs-a8703.appspot.com",
     messagingSenderId: "888059932087",
     appId: "1:888059932087:web:bfe6d62d9149c793a38c32"
-  };
+};
 //inicializacion Firebase
 firebase.initializeApp(firebaseConfig);
